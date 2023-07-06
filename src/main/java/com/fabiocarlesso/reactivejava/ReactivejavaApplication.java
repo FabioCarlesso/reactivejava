1fb56a8b564407d10b6d87796ce322ed212b3867
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-07-06 15:54:28 -0300

    Reactive spring webflux exemple

--- src/main/java/com/fabiocarlesso/reactivejava/ReactivejavaApplication.java
+++ src/main/java/com/fabiocarlesso/reactivejava/ReactivejavaApplication.java
@@ -1,13 +1,17 @@
 package com.fabiocarlesso.reactivejava;
 
+import com.fabiocarlesso.reactivejava.config.GreetingClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ReactivejavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactivejavaApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ReactivejavaApplication.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		System.out.println(">> message = " + greetingClient.getMessage().block());
 	}
 
 }
